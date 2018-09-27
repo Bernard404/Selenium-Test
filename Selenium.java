c8df8cf4ffe9afc04cda73ef57022a846575e31b
piratebernard <bernardsteemers@Nib.local>
2018-09-27 17:10:02 +0100

    .

--- Selenium.java
+++ Selenium.java
@@ -9,7 +9,7 @@ public class Selenium
 	@Test
 	public void test() 
 	{
-		System.setProperty("webdriver.gecko.driver", "/Users/bernardsteemers/Downloads/geckodriver-master");
+		System.setProperty("webdriver.gecko.driver", "/Users/usermuser/Downloads/geckodriver-master");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.youtube.com/watch?v=rJ4rNZGAzW8");
 		driver.quit();
