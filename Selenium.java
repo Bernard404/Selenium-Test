6b846377342c01bcd55f30b3575c32bd7d24cfab
piratebernard <bernardsteemers@Nib.local>
2018-09-27 16:53:14 +0100

    test

---
+++ Selenium.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Selenium 
+{
+
+	@Test
+	public void test() 
+	{
+		System.setProperty("webdriver.gecko.driver", "/Users/bernardsteemers/Downloads/geckodriver-master");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.youtube.com/watch?v=rJ4rNZGAzW8");
+		driver.quit();
+	}
+
+}
